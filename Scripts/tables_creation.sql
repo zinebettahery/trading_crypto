36bfa761f2bedc14b7f65a7f093c15d157e2e19e
zineb <zinebtahery7@gmail.com>
2025-12-24 10:21:32 +0100

    update ctration tabes

--- Scripts/tables_creation.sql
+++ Scripts/tables_creation.sql
@@ -15,9 +15,7 @@ CREATE  TABLE "public".paire_trading (
 	date_ouverture       date    ,
 	crypto_base          integer    ,
 	crypto_contre        integer    ,
-	CONSTRAINT pk_paire_trading PRIMARY KEY ( id_paire ),
-	CONSTRAINT fk_paire_trading_cryptomonnaies FOREIGN KEY ( crypto_base ) REFERENCES "public".cryptomonnaies( id_crypto )   ,
-	CONSTRAINT fk_paire_trading_cryptomonnaies_0 FOREIGN KEY ( crypto_contre ) REFERENCES "public".cryptomonnaies( id_crypto )   
+	CONSTRAINT pk_paire_trading PRIMARY KEY ( id_paire )
  );
 
 CREATE  TABLE "public".prix_marche ( 
@@ -26,8 +24,7 @@ CREATE  TABLE "public".prix_marche (
 	volume               decimal    ,
 	date_maj             date    ,
 	id_paire             integer    ,
-	CONSTRAINT pk_prix_marche PRIMARY KEY ( id_prix ),
-	CONSTRAINT fk_prix_marche_paire_trading FOREIGN KEY ( id_paire ) REFERENCES "public".paire_trading( id_paire )   
+	CONSTRAINT pk_prix_marche PRIMARY KEY ( id_prix )
  );
 
 CREATE  TABLE "public".statistique_marche ( 
@@ -37,8 +34,7 @@ CREATE  TABLE "public".statistique_marche (
 	periode              varchar(255)    ,
 	date_maj             date    ,
 	id_paire             integer    ,
-	CONSTRAINT pk_statistique_marche PRIMARY KEY ( id_statistique ),
-	CONSTRAINT fk_statistique_marche_paire_trading FOREIGN KEY ( id_paire ) REFERENCES "public".paire_trading( id_paire )   
+	CONSTRAINT pk_statistique_marche PRIMARY KEY ( id_statistique )
  );
 
 CREATE  TABLE "public".utilisateurs ( 
@@ -55,15 +51,13 @@ CREATE  TABLE "public".ordres (
 	id_order             integer  NOT NULL  ,
 	type_ordre           varchar(50)    ,
 	"mode"               varchar(100)    ,
-	quantite             integer    ,
+	quantite             decimal    ,
 	prix                 decimal    ,
 	statut               varchar(50)    ,
 	date_creation        date    ,
 	id_utilisateur       integer    ,
 	id_paire             integer    ,
-	CONSTRAINT pk_ordres PRIMARY KEY ( id_order ),
-	CONSTRAINT fk_ordres_utilisateurs FOREIGN KEY ( id_utilisateur ) REFERENCES "public".utilisateurs( id_utilisateur )   ,
-	CONSTRAINT fk_ordres_paire_trading FOREIGN KEY ( id_paire ) REFERENCES "public".paire_trading( id_paire )   
+	CONSTRAINT pk_ordres PRIMARY KEY ( id_order )
  );
 
 CREATE  TABLE "public".portefeuilles ( 
@@ -73,21 +67,17 @@ CREATE  TABLE "public".portefeuilles (
 	date_maj             date    ,
 	id_utilisateur       integer    ,
 	id_crypto            integer    ,
-	CONSTRAINT pk_portefeuilles PRIMARY KEY ( id_portefeuille ),
-	CONSTRAINT fk_portefeuilles_utilisateurs FOREIGN KEY ( id_utilisateur ) REFERENCES "public".utilisateurs( id_utilisateur )   ,
-	CONSTRAINT fk_portefeuilles_cryptomonnaies FOREIGN KEY ( id_crypto ) REFERENCES "public".cryptomonnaies( id_crypto )   
+	CONSTRAINT pk_portefeuilles PRIMARY KEY ( id_portefeuille )
  );
 
 CREATE  TABLE "public".trades ( 
 	id_trade             integer  NOT NULL  ,
 	prix                 decimal    ,
-	quantite             integer    ,
+	quantite             decimal    ,
 	date_execution       date    ,
 	id_order             integer    ,
 	id_paire             integer    ,
-	CONSTRAINT pk_trades PRIMARY KEY ( id_trade ),
-	CONSTRAINT fk_trades_ordres FOREIGN KEY ( id_order ) REFERENCES "public".ordres( id_order )   ,
-	CONSTRAINT fk_trades_paire_trading FOREIGN KEY ( id_paire ) REFERENCES "public".paire_trading( id_paire )   
+	CONSTRAINT pk_trades PRIMARY KEY ( id_trade )
  );
 
 CREATE  TABLE "public".audit_trail ( 
@@ -99,10 +89,7 @@ CREATE  TABLE "public".audit_trail (
 	id_utilisateur       integer    ,
 	id_order             integer    ,
 	id_trade             integer    ,
-	CONSTRAINT pk_audit_trail PRIMARY KEY ( id_audit ),
-	CONSTRAINT fk_audit_trail_utilisateurs FOREIGN KEY ( id_utilisateur ) REFERENCES "public".utilisateurs( id_utilisateur )   ,
-	CONSTRAINT fk_audit_trail_ordres FOREIGN KEY ( id_order ) REFERENCES "public".ordres( id_order )   ,
-	CONSTRAINT fk_audit_trail_trades FOREIGN KEY ( id_trade ) REFERENCES "public".trades( id_trade )   
+	CONSTRAINT pk_audit_trail PRIMARY KEY ( id_audit )
  );
 
 CREATE  TABLE "public".detection_anomalie ( 
@@ -112,8 +99,36 @@ CREATE  TABLE "public".detection_anomalie (
 	commentaire          text    ,
 	id_order             integer    ,
 	id_utilisateur       integer    ,
-	CONSTRAINT pk_detection_anomalie PRIMARY KEY ( id_detection ),
-	CONSTRAINT fk_detection_anomalie_ordres FOREIGN KEY ( id_order ) REFERENCES "public".ordres( id_order )   ,
-	CONSTRAINT fk_detection_anomalie_utilisateurs FOREIGN KEY ( id_utilisateur ) REFERENCES "public".utilisateurs( id_utilisateur )   
+	CONSTRAINT pk_detection_anomalie PRIMARY KEY ( id_detection )
  );
+
+ALTER TABLE "public".audit_trail ADD CONSTRAINT fk_audit_trail_utilisateurs FOREIGN KEY ( id_utilisateur ) REFERENCES "public".utilisateurs( id_utilisateur );
+
+ALTER TABLE "public".audit_trail ADD CONSTRAINT fk_audit_trail_ordres FOREIGN KEY ( id_order ) REFERENCES "public".ordres( id_order );
+
+ALTER TABLE "public".audit_trail ADD CONSTRAINT fk_audit_trail_trades FOREIGN KEY ( id_trade ) REFERENCES "public".trades( id_trade );
+
+ALTER TABLE "public".detection_anomalie ADD CONSTRAINT fk_detection_anomalie_ordres FOREIGN KEY ( id_order ) REFERENCES "public".ordres( id_order );
+
+ALTER TABLE "public".detection_anomalie ADD CONSTRAINT fk_detection_anomalie_utilisateurs FOREIGN KEY ( id_utilisateur ) REFERENCES "public".utilisateurs( id_utilisateur );
+
+ALTER TABLE "public".ordres ADD CONSTRAINT fk_ordres_utilisateurs FOREIGN KEY ( id_utilisateur ) REFERENCES "public".utilisateurs( id_utilisateur );
+
+ALTER TABLE "public".ordres ADD CONSTRAINT fk_ordres_paire_trading FOREIGN KEY ( id_paire ) REFERENCES "public".paire_trading( id_paire );
+
+ALTER TABLE "public".paire_trading ADD CONSTRAINT fk_paire_trading_cryptomonnaies FOREIGN KEY ( crypto_base ) REFERENCES "public".cryptomonnaies( id_crypto );
+
+ALTER TABLE "public".paire_trading ADD CONSTRAINT fk_paire_trading_cryptomonnaies_0 FOREIGN KEY ( crypto_contre ) REFERENCES "public".cryptomonnaies( id_crypto );
+
+ALTER TABLE "public".portefeuilles ADD CONSTRAINT fk_portefeuilles_utilisateurs FOREIGN KEY ( id_utilisateur ) REFERENCES "public".utilisateurs( id_utilisateur );
+
+ALTER TABLE "public".portefeuilles ADD CONSTRAINT fk_portefeuilles_cryptomonnaies FOREIGN KEY ( id_crypto ) REFERENCES "public".cryptomonnaies( id_crypto );
+
+ALTER TABLE "public".prix_marche ADD CONSTRAINT fk_prix_marche_paire_trading FOREIGN KEY ( id_paire ) REFERENCES "public".paire_trading( id_paire );
+
+ALTER TABLE "public".statistique_marche ADD CONSTRAINT fk_statistique_marche_paire_trading FOREIGN KEY ( id_paire ) REFERENCES "public".paire_trading( id_paire );
+
+ALTER TABLE "public".trades ADD CONSTRAINT fk_trades_ordres FOREIGN KEY ( id_order ) REFERENCES "public".ordres( id_order );
+
+ALTER TABLE "public".trades ADD CONSTRAINT fk_trades_paire_trading FOREIGN KEY ( id_paire ) REFERENCES "public".paire_trading( id_paire );
 
