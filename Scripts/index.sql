23cdaf8180094bb7d698d98093854b3efdaf2af3
safiyadaoudi01 <safiya.daoudi29@gmail.com>
2025-12-25 12:50:45 +0100

    ajout des index sur la table paire_trading de type B-Tree et covering et partiel

--- Scripts/index.sql
+++ Scripts/index.sql
@@ -1,10 +1,25 @@
--- 1. Index B-tree classique pour les recherches fréquentes sur id_order ou id_utilisateur ou date_detection
+-- Index B-tree classique pour les recherches fréquentes sur id_order ou id_utilisateur ou date_detection
 CREATE INDEX idx_detection_anomalie_utilisateur ON detection_anomalie(id_utilisateur);
 CREATE INDEX idx_detection_anomalie_order ON detection_anomalie(id_order);
 CREATE INDEX idx_detection_anomalie_date ON detection_anomalie(date_detection);
 
 
--- 2. Index covering si tu as des requêtes qui récupèrent plusieurs colonnes fréquemment
+-- Index covering si tu as des requêtes qui récupèrent plusieurs colonnes fréquemment
 -- Exemple : récupérer date_detection et commentaire pour un id_utilisateur
 CREATE INDEX idx_detection_anomalie_utilisateur_covering 
 ON detection_anomalie(id_utilisateur) INCLUDE(date_detection, commentaire);
+
+-- B-tree pour les recherches par statut, date_ouverture ou crypto
+CREATE INDEX idx_paire_trading_statut ON paire_trading(statut);
+CREATE INDEX idx_paire_trading_date ON paire_trading(date_ouverture);
+CREATE INDEX idx_paire_trading_crypto_base ON paire_trading(crypto_base);
+CREATE INDEX idx_paire_trading_crypto_contre ON paire_trading(crypto_contre);
+
+-- Covering index si tu sélectionnes souvent statut et crypto_contre pour une crypto_base
+CREATE INDEX idx_paire_trading_base_covering 
+ON paire_trading(crypto_base) INCLUDE(statut,crypto_contre);
+
+-- Partial index pour les paires actives
+CREATE INDEX idx_paire_trading_ouvertes 
+ON paire_trading(date_ouverture) 
+WHERE statut = 'ACTIVE';
